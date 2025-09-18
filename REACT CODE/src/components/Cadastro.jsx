21549e50c293222f632bbd75c40b30c848f0e61b
vinicios <dantas.vinicios@escolar.ifrn.edu.br>
2025-09-18 14:24:53 -0300

    react home-layout

---
+++ REACT CODE/src/components/Cadastro.jsx
@@ -0,0 +1,12 @@
+const Cadastro = () => {
+  return (
+    <div>
+        <main class="main-content">
+            <div class="content-text">
+            </div>
+        </main>
+    </div>
+  );
+};
+
+export default Cadastro;
