b109d8b2b69c83d5062e85371d6088d7db481299
lourranny <santana.lourranny@escolar.ifrn.edu.br>
2025-09-25 14:03:09 -0300

    rotas cadastro e login

--- HTML CODE/main.py
+++ HTML CODE/main.py
@@ -7,10 +7,10 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# ROTAS EXISTENTES (HOME e INDEX)
 @app.get("/")
 def home(request: Request):
     nome = "Visitante"
-    
     return templates.TemplateResponse(
         "home.html",
         {"request": request, "usuario_nome": nome}
@@ -18,8 +18,33 @@ def home(request: Request):
 
 @app.get("/index")
 def index(request: Request):
-    
     return templates.TemplateResponse(
         "index.html",
+        {"request": request}
+    )
+
+# ----------------------------------------------------------------------
+# NOVAS ROTAS PARA LOGIN E CADASTRO
+# ----------------------------------------------------------------------
+
+@app.get("/login")
+def login_page(request: Request):
+    """
+    Rota para a página de Login.
+    URL: http://127.0.0.1:8000/login
+    """
+    return templates.TemplateResponse(
+        "login.html",  # Rendeiza o HTML do Login
+        {"request": request}
+    )
+
+@app.get("/cadastro")
+def cadastro_page(request: Request):
+    """
+    Rota para a página de Cadastro.
+    URL: http://127.0.0.1:8000/cadastro
+    """
+    return templates.TemplateResponse(
+        "cadastro.html",  # Rendeiza o HTML do Cadastro
         {"request": request}
     )
