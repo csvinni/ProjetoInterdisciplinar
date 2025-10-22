423380c406f76024c7c2eb8320213a162b53ce03
Maria Clara <brito.clara@escolar.ifrn.edu.br>
2025-10-22 08:37:04 -0300

    add historico

--- projeto/controllers/dashboard.py
+++ projeto/controllers/dashboard.py
@@ -10,4 +10,4 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
 def dashboard_home(request: Request, session: Session = Depends(get_session)):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse("historico.html", {"request": request})
